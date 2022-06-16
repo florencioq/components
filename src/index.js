c26d372e44679115b1f0e60888f8d9e927619dc4
Florencio <florencio@ideos.com.br>
2022-06-16 09:13:03 -0300

    avanço

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+import faker from 'faker';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
-            <CommentDetail author="Alex" timeAgo="Today at 2:00PM" />
-            <CommentDetail author="Jane" timeAgo="Yesterday at 10:00AM" />
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" avatar={faker.image.image()} />
+            </ApprovalCard>
+            <CommentDetail author="Alex" timeAgo="Today at 2:00PM" content="Very nice post" avatar={faker.image.image()} />
+            <CommentDetail author="Jane" timeAgo="Yesterday at 10:00AM" content="Great post" avatar={faker.image.image()} />
         </div>
     );
 };
